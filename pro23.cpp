ecdb3cdfda0c2b71fba21be339afc965d92597c4
Rishabh Jaiswal <rishabhj1024@gmail.com>
2016-01-19 14:07:13 +0530

    Update pro23.cpp
    Creating Binary Search tree

--- pro23.cpp
+++ pro23.cpp
@@ -9,9 +9,11 @@ struct node{
 };
 
 struct node *root = NULL;
+struct node *p = NULL;
+struct node *pr = NULL;
 int count = 0;
 
-void tree(){
+void tree_ins(){
 	node *n;
 	n = new node;
 	n->l = NULL;
@@ -21,6 +23,34 @@ void tree(){
 	cin>>data;
 	n->info = data;
 	if(count==0){
-		
+		root = n;
+	} else{
+		p = root;
+		while(p!=NULL){
+			if(p->info <= data){
+				pr = p;
+				p = p->r;
+			}
+			if(p->info > data){
+				pr = p;
+				p = p->l;
+			}
+		}
+		if(pr->info <= data){
+				pr->r = n;
+			}
+			if(pr->info > data){
+				pr->l = n;
+			}
 	}
+}
+
+int main(){
+	int c=1;
+	while(c!=0){
+		tree_ins();
+		cout<<"Want to add another value? (1=yes, 0=no)";
+		cin>>c;
+	}
+	return 0;
 }
