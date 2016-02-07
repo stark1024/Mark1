44bba4af175cba113ded037524de4d503b30fb5a
Rishabh Jaiswal <rishabhj1024@gmail.com>
2016-02-07 15:27:43 +0530

    Update pro25.cpp
    The 2 finger algorithm used in merge sort
    (Still some error in new test case)

--- pro25.cpp
+++ pro25.cpp
@@ -2,11 +2,11 @@
 using namespace std;
 
 int main(){
-	int a[5] = {9, 7, 5, 2, 1};
-	int b[5] = {8, 6, 4, 3, 0};
+	int a[5] = {88, 27, 5, 2, 1};
+	int b[5] = {9, 6, 4, 3, 1};
 	int c[10];
-	int i,j = 4, k=0;
-	while(i>0 && j>0){
+	int i=4 ,j = 4, k=0;
+	while(i!=0 || j!=0){
 		if(a[i]<=b[j]){
 			c[k] = a[i];
 			k++;
@@ -19,9 +19,38 @@ int main(){
 				j--;
 		}
 	}
-	for(i=0; i<10; i++){
-		cout<<c[i]<<"\t";
+	if(i==0){
+		for(i=j; i>=0; i--){
+			j=0;
+			if(a[j]<=b[i] && j==0){
+				c[k] = a[0];
+				k++;
+				j--;
+			} else{
+				c[k] = b[i];
+				k++;
+			}
+		}
 	}
-	cin>>d;
+	if(j==0){
+		for(j=i; j>=0; j--){
+			i=0;
+			if(a[j]>=b[i] && i==0){
+				c[k] = b[0];
+				k++;
+				i--;
+			} else{
+				c[k] = a[j];
+				k++;
+			}
+		}
+	}
+	int d=1;
+	while(d!=0){
+		for(i=0; i<10; i++){
+			cout<<c[i]<<"\t";
+		}
+		cin>>d;
+	}
 return 0;
 }
